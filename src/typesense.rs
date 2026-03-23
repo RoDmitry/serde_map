35745ceb2df37947db38ecd86f10463ed7ab9130
RoDmitry <gh@rdmtr.com>
2026-03-23 19:56:50 +0000

    Release 0.3.0 + Fix Typesense

--- src/typesense.rs
+++ src/typesense.rs
@@ -1,6 +1,6 @@
 use crate::{SerdeMap, SerdeMapStrategy};
 
-impl<K, V, KS: SerdeMapStrategy<K>> typesense::field::ToTypesenseField for SerdeMap<K, V, KS> {
+impl<K, V, KS: SerdeMapStrategy<K>> typesense::prelude::ToTypesenseField for SerdeMap<K, V, KS> {
     #[inline(always)]
     fn to_typesense_type() -> &'static str {
         "object"
